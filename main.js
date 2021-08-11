4fd209279b55770f5ff57bd1452e60d193622a56
Niv Ivri <nivii123456@gmail.com>
2021-08-11 16:43:36 +0300

    text on the pic

--- main.js
+++ main.js
@@ -16,13 +16,11 @@ function onInit() {
 }
 
 
-
-
 function drawImg(elImg) {
 
     //remove gallery
-    document.querySelector ('.main-content-gallery').classList.add('hidden-section')
-    document.querySelector ('.main-content-editor').classList.remove('hidden-section')
+    document.querySelector('.main-content-gallery').classList.add('hidden-section')
+    document.querySelector('.main-content-editor').classList.remove('hidden-section')
     //create mem
     onCerteMeme(elImg);
     console.log(elImg);
@@ -53,19 +51,104 @@ function onChangeText(text) {
 }
 
 
-function drawText(txt, x, y) {
-    gCtx.fillText(txt, x, y);
+
+
+function drawText(text, size, align, color) {
+    var currMeme = getgMeme()
+    var textInput = document.querySelector("[name='text-box']").value
+    //gCtx.font = `${size}px Impact`
+    gCtx.font = `60px Impact`
+    gCtx.fillStyle = color
+    gCtx.strokeStyle = 'balck'
     gCtx.lineWidth = 1
-    gCtx.strokeStyle = 'red'
-    gCtx.fillStyle = 'white'
-    gCtx.font = '48px Impact, Haettenschweiler, Arial Narrow Bold, sans-serif'
-    gCtx.fillText(txt, x, y)
-    gCtx.strokeText(txt, x, y)
+    gCtx.align = align
+    if (currMeme.selectedLineIdx === 1) {
+        gCtx.textBaseline = 'top'
+        gCtx.fillText(text, gElCanvas.width / 2, 0, gElCanvas.width)
+        gCtx.strokeText(text, gElCanvas.width / 2, 0, gElCanvas.width)
+    }
+    else if (currMeme.selectedLineIdx === 2) {
+        gCtx.textBaseline = 'bottom'
+        debugger
+        gCtx.fillText(text, gElCanvas.width / 2, gElCanvas.height, gElCanvas.width)
+        gCtx.strokeText(text, gElCanvas.width / 2, gElCanvas.height, gElCanvas.width)
+    }
+    else {
+        gCtx.textBaseline = 'top'
+        gCtx.fillText(text, gElCanvas.width / 2, gElCanvas.height / 2, gElCanvas.width)
+        gCtx.strokeText(text, gElCanvas.width / 2, gElCanvas.height / 2, gElCanvas.width)
+    }
 }
-function canvasClicked(ev)
-{
-    console.log('ev', ev);
+
+function onIncreaseText() {
+    //gCtx.clearRect(0, 0, gElCanvas.width, gElCanvas.height)
+    increaseText()
+}
+
+
+
+function canvasClicked(ev) {
+    //var meme = getgMeme().lines
+    //meme.forEach(text => {
+    //    var rect =text.rect
+    //    gCtx.rect(...rect)
+    //    gCtx.stroke()
+
+    //});
+    //var rect = meme.lines[0].rect
+    //console.log(rect);
+
+
+
+
+    ////const clickedStar = meme.lines.find(line => {
+    ////    console.log( line.x + 'line.x');
+    ////    console.log( line.y + 'line.y');
+    ////    debugger
+    ////    return ev.offsetX+10 >= line.x && ev.offsetX <= line.x + 100 &&
+    ////    ev.offsetY+10 >= line.y && ev.offsetY <= line.y+100
+    ////})
+
+    ////if (clickedStar) openModal(console.log('inside'))
 
 }
 
 
+
+//function wrapText(context, text, x, y, maxWidth, lineHeight) {
+//    var words = text.split(' ');
+//    var line = '';
+
+//    for(var n = 0; n < words.length; n++) {
+//      var testLine = line + words[n] + ' ';
+//      var metrics = context.measureText(testLine);
+//      var testWidth = metrics.width;
+//      if (testWidth > maxWidth && n > 0) {
+//        context.fillText(line, x, y);
+//        line = words[n] + ' ';
+//        y += lineHeight;
+//      }
+//      else {
+//        line = testLine;
+//      }
+//    }
+//    context.fillText(line, x, y);
+//  }
+
+//  var canvas = document.getElementById('myCanvas');
+//  var context = canvas.getContext('2d');
+//  var maxWidth = 400;
+//  var lineHeight = 25;
+//  var x = (canvas.width - maxWidth) / 2;
+//  var y = 60;
+//  var text = 'All the world \'s a stage, and all the men and women merely players. They have their exits and their entrances; And one man in his time plays many parts.';
+
+//  context.font = '16pt Calibri';
+//  context.fillStyle = '#333';
+
+//  wrapText(context, text, x, y, maxWidth, lineHeight);
+
+
+function renderText() {
+
+}
--- meme.service.js
+++ meme.service.js
@@ -2,19 +2,7 @@
 var gKeywords = { 'happy': 12, 'funny puk': 1 }
 var gImgs = [{ id: 1, url: 'img/popo.jpg', keywords: ['happy'] }];
 
-var gMeme = {
-    selectedImgId: 5,
-    selectedLineIdx: 0,
-    lines: [
-        {
-            txt: 'I never eat Falafel',
-            size: 20,
-            align: 'left',
-            color: 'red'
-        }
-    ]
-}
-
+var gMeme;
 
 function createMeme(imgIdx) {
     gMeme = {
@@ -22,25 +10,25 @@ function createMeme(imgIdx) {
         selectedLineIdx: 0,
         lines: [
             {
-                txt: '',
+                txt: 'I never eat Falafel',
                 size: 20,
                 align: 'left',
                 color: 'red'
             }
+
         ]
     }
 }
 
 
 function getgMeme() {
-    return gMemel;
+    return gMeme;
 }
 
 function AddText(text) {
     gMeme.selectedLineIdx++;
     var x
     var y
-    console.log(gMeme.selectedLineIdx);
     switch (gMeme.selectedLineIdx) {
         case 1:
             x = 10
@@ -55,7 +43,27 @@ function AddText(text) {
             y = 245
             break;
     }
-    drawText(text, x, y)
+    gMeme.lines.push({
+        txt: text,
+        size: 20,
+        align: 'left',
+        color: 'red',
+        x,
+        y
+    })
+    var size = gMeme.lines[gMeme.selectedLineIdx - 1].size
+    var align = gMeme.lines[gMeme.selectedLineIdx - 1].align
+    var color = gMeme.lines[gMeme.selectedLineIdx - 1].color
+    drawText(text, x, y, size, align, color)
 }
 
 
+function increaseText() {
+    var size = gMeme.lines[gMeme.selectedLineIdx - 1].size + 3;
+    var text = gMeme.lines[gMeme.selectedLineIdx - 1].text
+    var align = gMeme.lines[gMeme.selectedLineIdx - 1].align
+    var color = gMeme.lines[gMeme.selectedLineIdx - 1].color
+    var x = gMeme.lines[gMeme.selectedLineIdx - 1].x
+    var y = gMeme.lines[gMeme.selectedLineIdx - 1].y
+    drawText(text, x, y, size, align, color)
+}
